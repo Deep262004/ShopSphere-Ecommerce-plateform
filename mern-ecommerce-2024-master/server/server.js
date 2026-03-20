73e6e06ba169e0637758b3bd5b4289e0a59b7ca1
Deep Nandan Singh <deepnandansingh381@gmail.com>
2026-03-20 14:19:58 +0530

    Add root route to confirm API is running

--- mern-ecommerce-2024-master/server/server.js
+++ mern-ecommerce-2024-master/server/server.js
@@ -55,5 +55,7 @@ app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
